4f945c31c43e147c37243f555b67b28ae1180052
Christian Bandoquillo <bandoquillo.c33@gmail.com>
2019-09-26 13:33:30 +0800

    CCFC Version 1.01

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return route('login');
+        return view('home');
     }
 }
--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return route('login');
+            return redirect('/home');
         }
 
         return $next($request);
