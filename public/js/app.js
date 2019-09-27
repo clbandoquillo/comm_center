deab7d10347563230ffe88f3bc442cc669524dde
Christian Bandoquillo <bandoquillo.c33@gmail.com>
2019-09-27 12:56:43 +0800

    app.js

--- public/js/app.js
+++ public/js/app.js
@@ -70168,8 +70168,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Git\comm_center\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Git\comm_center\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\sites\CCFC\comm_center\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\sites\CCFC\comm_center\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
